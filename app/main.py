2a372f45dd6134f4f50abe5e5c41b13286da7cdb
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-28 15:45:18 +0545

    wireup stdout & stderr

--- app/main.py
+++ app/main.py
@@ -3,16 +3,14 @@ import sys
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
     command = sys.argv[3]
     args = sys.argv[4:]
 
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    if completed_process.stdout:
+        print(completed_process.stdout.decode("utf-8"), end="")
+    elif completed_process.stderr:
+        print(completed_process.stderr.decode("utf-8"), file=sys.stderr, end="")
 
 
 if __name__ == "__main__":
