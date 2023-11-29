b764078d24a2635ccdeb93caef71a910203d0e20
Rabin Dhamala <rabinkmc@gmail.com>
2023-11-29 10:56:21 +0545

    filesystem isolation

--- app/main.py
+++ app/main.py
@@ -1,11 +1,25 @@
 import subprocess
+import os
 import sys
+import tempfile
 
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
+    TEMP_DIR = tempfile.mkdtemp()
+    directory = TEMP_DIR + command[: command.rfind("/")]
+    os.makedirs(directory, exist_ok=True)
+    subprocess.run(
+        [
+            "cp",
+            "-r",
+            command,
+            directory,
+        ]
+    )
+    os.chroot(TEMP_DIR)
     completed_process = subprocess.run([command, *args], capture_output=True)
     if completed_process.stdout:
         print(completed_process.stdout.decode("utf-8"), end="")
